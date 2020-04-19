cdc2d8c7a488c60903797d15bc8f869cac340037
Shahin <shh8362@gmail.com>
2020-04-19 17:02:24 +0100

    Improve BiNom methods accuracy

--- QSharpCheck 1.1/Tool/BiNom.cs
+++ QSharpCheck 1.1/Tool/BiNom.cs
@@ -19,11 +19,6 @@ namespace QSharpCheck
                 LessThanEqual20.Add(i, Math.Log(p *= i));
         }
 
-        // It’s usually more convenient to work with the logarithms of factorials
-        // rather than factorials themselves. Since n! = 1 × 2 × 3 × … × n, log(n!) = log(1) + log(2) + log(3) + … + log(n).
-        // That gives a way to compute log(n!), but it’s slow for large arguments: the run time is proportional to the size of n.
-        // we tabulate log(n!) for n up to some size and then use a formula to calculate log(n!) for larger arguments that 
-        // is Stirling approximation.
         private static double LogOfFac(int num)
         {
             // This is used when the number is not larger than 20
@@ -52,8 +47,6 @@ namespace QSharpCheck
             return Math.Exp(result);
         }
 
-        // The boolean 'lowerTail' is to help whether
-        // to do the one-tailed test or the two-tailed test
         public int QBinom(double significanceLevel, int n, double p, bool lowerTail)
         {
             var criticalVal = 0;
@@ -63,12 +56,6 @@ namespace QSharpCheck
                 while (true)
                 {
                     dBinomResult += DBinom(criticalVal, n, p);
-                    // if (dBinomResult == 0)
-                    // {
-                    //     criticalVal += n;
-                    //     break;
-                    // }
-
                     if (dBinomResult >= significanceLevel) break;
                     criticalVal++;
                 }
